aa0ff2420b8e7c4ff9c879d68f60b8baa6b92619
Santosh Passoubady <santoshpassoubady@gmail.com>
2024-11-30 15:40:36 -0500

    more memecoin stuff

--- src/routes/wallet/+page.svelte
+++ src/routes/wallet/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import ProtectedRoute from '$lib/components/ProtectedRoute.svelte';
-	
+	import { walletSetup } from '$lib/utils/wallet';
+
 	export let data;
 	const { user } = data;
+	console.log('user', data.user);
 </script>
 
 <ProtectedRoute>
@@ -10,6 +12,13 @@
 		<h1>Wallet Page</h1>
 		{#if user}
 			<p>Portfolio: {JSON.stringify(user.portfolio)}</p>
+
+			<button
+				class="rounded-lg bg-neutral-800 px-4 py-2 font-medium text-gray-100 transition-colors hover:bg-neutral-700"
+				on:click={() => walletSetup(user.clerkId)}
+			>
+				click to setup wallet
+			</button>
 		{/if}
 	</div>
 </ProtectedRoute>
